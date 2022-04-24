7d9846fde5889205277900a5728c9b75c3f99970
Felix <felix.schmid@siriusonline.de>
2022-04-24 17:31:44 +0200

    initial commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'fes-watch-sync',
+    appId: '1:1046696012009:web:870379ac905af3558c2878',
+    storageBucket: 'fes-watch-sync.appspot.com',
+    apiKey: 'AIzaSyDupaUyfTzHsx-SvehbbuVk4Z08HiIOoZ8',
+    authDomain: 'fes-watch-sync.firebaseapp.com',
+    messagingSenderId: '1046696012009',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'fes-watch-sync',
+    appId: '1:1046696012009:web:870379ac905af3558c2878',
+    storageBucket: 'fes-watch-sync.appspot.com',
+    apiKey: 'AIzaSyDupaUyfTzHsx-SvehbbuVk4Z08HiIOoZ8',
+    authDomain: 'fes-watch-sync.firebaseapp.com',
+    messagingSenderId: '1046696012009',
+  },
   production: false
 };
 
